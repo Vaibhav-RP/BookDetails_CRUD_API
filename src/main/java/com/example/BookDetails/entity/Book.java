2ef841de515a5fcbbdeb0f04b6b8077914b741f5
Vaibhav-RP <vrp8698231262@gmail.com>
2023-05-26 16:07:28 +0530

    initial commit

---
+++ src/main/java/com/example/BookDetails/entity/Book.java
@@ -0,0 +1,19 @@
+package com.example.BookDetails.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String description;
+
+}
---
+++ src/test/java/com/example/BookDetails/BookDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookDetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
